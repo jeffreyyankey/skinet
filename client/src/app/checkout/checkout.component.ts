62168db162ae7dfb8179c827d5c935d36157cdfe
Jeffrey Yankey <jeffrey.yankey@outlook.com>
2024-02-22 22:16:09 -0500

    End of Section 21

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../account/account.service';
+import { BasketService } from '../basket/basket.service';
 
 @Component({
   selector: 'app-checkout',
@@ -9,10 +10,15 @@ import { AccountService } from '../account/account.service';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor(private fb: FormBuilder, private acccountService: AccountService) {}
+  constructor(
+    private fb: FormBuilder, 
+    private acccountService: AccountService,
+    private basketService: BasketService
+  ) {}
   
   ngOnInit(): void {
     this.getAddressFormValues();
+    this.getDeliveryMethodValue();
   }
 
   checkoutForm = this.fb.group({
@@ -28,7 +34,7 @@ export class CheckoutComponent implements OnInit {
       deliveryMethod: ['', Validators.required]
     }),
     paymentForm: this.fb.group({
-      nameOnCare: ['', Validators.required]
+      nameOnCard: ['', Validators.required]
     })
   })
 
@@ -38,5 +44,12 @@ export class CheckoutComponent implements OnInit {
         address && this.checkoutForm.get('addressForm')?.patchValue(address)
       }
     })
+  }
+
+  getDeliveryMethodValue() {
+    const basket = this.basketService.getCurrentBasketValue();
+    if (basket && basket.deliveryMethodId) {
+      this.checkoutForm.get('deliveryForm')?.get('deliveryMethod')?.patchValue(basket.deliveryMethodId.toString());
+    }
   }
 }
